29683fbab4e0f6613e18af2989edb0509cdf6a72
SOSTheBlack <jeancesargarcia@gmail.com>
2022-03-26 15:08:40 -0300

    criado resource da api do ponto de intesse

---
+++ app/Http/Resources/PointInterestResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\PointInterest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin PointInterest
+ */
+class PointInterestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     *
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'opened' => $this->opened?->format('H:i'),
+            'closed' => $this->closed?->format('H:i'),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
