0d755074a91c320f7d28c58d7b2092dcf9be68f6
SOSTheBlack <jeancesargarcia@gmail.com>
2022-03-26 15:04:01 -0300

    criado migração do ponto de interesse

---
+++ database/migrations/2022_03_25_144153_create_point_interests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+/**
+ * Class CreatePointInterestsTable.
+ */
+class CreatePointInterestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('point_interests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignIdFor(User::class);
+            $table->uuid();
+            $table->string('name');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->time('opened')->nullable();
+            $table->time('closed')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::drop('point_interests');
+    }
+}
