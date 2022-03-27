f1c61d2c7ae263f2a674de4abaa88291e2237cf8
SOSTheBlack <jeancesargarcia@gmail.com>
2022-03-27 00:34:41 -0300

    service para regra de negocio

---
+++ app/Services/Proximity/ProximityService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\Proximity;
+
+use App\Models\Approximation;
+use App\Models\PointInterest;
+use App\Repositories\Contracts\PointInterestRepository;
+use App\Services\Proximity\Contracts\ProximityContract;
+use Illuminate\Database\Eloquent\Collection;
+
+class ProximityService implements ProximityContract
+{
+    private PointInterestRepository $pointInterestRepository;
+
+    /**
+     * @param  PointInterestRepository  $pointInterestRepository
+     */
+    public function __construct(PointInterestRepository $pointInterestRepository)
+    {
+        $this->pointInterestRepository = $pointInterestRepository;
+    }
+
+    public function search(Approximation $approximation): Collection
+    {
+        return $this->pointInterestRepository->searchProximity($approximation->latitude, $approximation->longitude, $approximation->meters);
+    }
+}
